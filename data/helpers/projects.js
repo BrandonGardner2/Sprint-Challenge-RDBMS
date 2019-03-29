3a1e0bb85697aeffaa3e8bd8b1123d42fef3dec8
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-29 10:54:42 -0500

    Project by id returns correct form with actions

--- data/helpers/projects.js
+++ data/helpers/projects.js
@@ -1,11 +1,23 @@
 const db = require("../config");
 
 module.exports = {
-  addProject
+  addProject,
+  getProjectById
 };
 
-function getProjectById(id) {
-  return; //stuff here
+async function getProjectById(id) {
+  const project = await db("projects")
+    .where({ id })
+    .first();
+  const actions = await db("actions").where({ project_id: id });
+  if (project) {
+    return {
+      ...project,
+      actions
+    };
+  } else {
+    return false;
+  }
 }
 
 async function addProject(project) {
--- routes/projects-route.js
+++ routes/projects-route.js
@@ -4,11 +4,9 @@ const Projects = require("../data/helpers/projects");
 router.route("/").post(async (req, res) => {
   const project = req.body;
   if (!project.name || !project.description) {
-    res
-      .status(400)
-      .json({
-        message: "Please provide a name and description for the project."
-      });
+    res.status(400).json({
+      message: "Please provide a name and description for the project."
+    });
   }
   try {
     const newProject = await Projects.addProject(project);
@@ -20,6 +18,23 @@ router.route("/").post(async (req, res) => {
   }
 });
 
-router.route("/:id").get(async (req, res) => {});
+router.route("/:id").get(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const project = await Projects.getProjectById(id);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res.status(404).json({ message: "Project could not be located." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message:
+          "Something went wrong trying to retrieve that project information."
+      });
+  }
+});
 
 module.exports = router;
